92fcf86e32cc4add5c3c4671ca1c09001fbc326b
Vlad <admin@vladiator.biz>
2017-04-11 01:12:04 +0300

    chart ui

--- src/net/falsetrue/heapwalker/ui/Chart.java
+++ src/net/falsetrue/heapwalker/ui/Chart.java
@@ -2,13 +2,70 @@ package net.falsetrue.heapwalker.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.List;
 
+@SuppressWarnings("UseJBColor")
 public class Chart extends JPanel {
+    private static final int CHART_WIDTH_PERCENT = 94;
+    private static final int CHART_MARGIN_PERCENT = (100 - CHART_WIDTH_PERCENT) / 2;
+
     private List<Item> data;
+    private int hovered = -1;
+    private int chartSize;
 
     public Chart() {
         setPreferredSize(new Dimension(400, getHeight()));
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int x = e.getX() - chartSize / 2;
+                int y = e.getY() - chartSize / 2;
+                int newHovered = -1;
+                if (x * x + y * y <= chartSize * chartSize) {
+                    int angle = (int) (180 * Math.atan2(-y, x) / Math.PI);
+                    if (angle < 0) {
+                        angle = 360 + angle;
+                    }
+                    newHovered = -1;
+                    for (int i = 0; i < data.size(); i++) {
+                        if (data.get(i).startAngle + data.get(i).angle > angle) {
+                            newHovered = i;
+                            break;
+                        }
+                    }
+                    if (newHovered == -1) {
+                        newHovered = data.size() - 1;
+                    }
+                }
+                if (newHovered != hovered) {
+                    hovered = newHovered;
+                    updateUI();
+                }
+            }
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                hovered = -1;
+                updateUI();
+            }
+        });
+    }
+
+    @Override
+    public void setSize(Dimension d) {
+        super.setSize(d);
+        chartSize = (int) Math.min(d.getWidth(), d.getHeight());
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(width, height);
+        chartSize = Math.min(width, height);
     }
 
     @Override
@@ -18,34 +75,55 @@ public class Chart extends JPanel {
             ((Graphics2D) g).setRenderingHint(
                 RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-            int sum = data.stream().mapToInt(Item::getCount).sum();
-            int lastAngle = 0;
-            int size = Math.min(getWidth(), getHeight());
-            int to = data.size() - 1;
-            g.setColor(data.get(to).color);
-            g.fillArc(0, 0, size, size, 0, 360);
-            for (int i = 0; i < to; i++) {
+            chartSize = Math.min(getWidth(), getHeight());
+            g.setColor(data.get(data.size() - 1).color);
+            int smallSize = chartSize * CHART_WIDTH_PERCENT / 100;
+            int smallStart = chartSize * CHART_MARGIN_PERCENT / 100;
+            g.fillArc(smallStart, smallStart, smallSize, smallSize, 0, 360);
+            for (int i = 0; i < data.size(); i++) {
                 Item item = data.get(i);
                 if (item.count == 0) {
                     continue;
                 }
-                int angle = item.count * 360 / sum;
                 g.setColor(item.color);
-                g.fillArc(0, 0, size, size, lastAngle - 1, angle + 1);
-                lastAngle += angle;
+                if (hovered == i) {
+                    g.fillArc(0, 0, chartSize, chartSize, item.startAngle, item.angle);
+                } else {
+                    g.fillArc(smallStart, smallStart, smallSize, smallSize, item.startAngle, item.angle);
+                }
             }
         }
     }
 
+    public void clear() {
+        data = null;
+        repaint();
+    }
+
     public void setData(List<Item> data) {
         this.data = data;
-        updateUI();
+        if (data.size() > 0) {
+            int sum = data.stream().mapToInt(Item::getCount).sum();
+            int lastAngle = 0;
+            for (Item item : data) {
+                if (item.count == 0) {
+                    continue;
+                }
+                item.angle = item.count * 360 / sum;
+                item.startAngle = lastAngle;
+                lastAngle += item.angle;
+            }
+            data.get(data.size() - 1).angle = 360 - data.get(data.size() - 1).startAngle;
+        }
+        repaint();
     }
 
     public static class Item {
         private String label;
         private int count;
         private Color color;
+        private int startAngle;
+        private int angle;
 
         public Item(String label, int count, Color color) {
             this.label = label;
